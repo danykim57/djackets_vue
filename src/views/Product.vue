8154479ade142ba9b1143a62c37c937acc09c600
danykim57 <55152550+danykim57@users.noreply.github.com>
2021-06-11 22:55:38 +0900

    Update Product.vue

---
+++ src/views/Product.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="page-product">
+        <div class="columns is-multiline">
+            <div class="column is-9">
+                <figure class="image mb-6">
+                    <img v-bind:src="product.get_image">
+                </figure>
+
+                <h1 class="title"> {{product.name}}</h1>
+                <p>{{product.description}} </p>
+            </div>
+            <div class="column is-3">
+                <h2 class="subtitle"-Information</h2>
+                <p><strong>Price: </strong>${{product.price}}</p>
+                <div class="field has-addons mt-6">
+                    <div class="control">
+                        <input type="number" class="input" min-"1" v-model="quantity">
+                    </div>
+                    <div class="control">
+                        <a class="buttom is-dark">Add to cart</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    name: 'Product',
+    data() {
+        return {
+
+        }
+    }
+}
+</script>

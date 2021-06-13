5894bcff70c6d5c1f1e1c0ade020adb1b244ac87
danykim57 <55152550+danykim57@users.noreply.github.com>
2021-06-13 20:28:26 +0900

    updated

--- src/store/index.js
+++ src/store/index.js
@@ -2,6 +2,12 @@ import { createStore } from 'vuex'
 
 export default createStore({
   state: {
+    cart: {
+      item: [],
+    },
+    isAuthenticated: false,
+    token: '',
+    isLoading: false
   },
   mutations: {
   },

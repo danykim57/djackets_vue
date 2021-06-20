c34a199615eaa24958757ada039ec4d84230139e
danykim57 <55152550+danykim57@users.noreply.github.com>
2021-06-20 18:14:06 +0900

    updated

--- src/views/Product.vue
+++ src/views/Product.vue
@@ -38,11 +38,12 @@ export default {
         this.getProduct()
     },
     methods: {
-        getProducts() {
+        async getProducts() {
+            this.$store.commit('setIsLoading', true)
             const category_slug = this.$route.params.category_slug
             const product_slug = this.$route.params.product_slug
 
-            axios
+            await axios
                 .get('/api/v1/products/$(category_slug)/$(product_slug)')
                 .then(response => {
                     this.product = response.data
@@ -50,6 +51,7 @@ export default {
                 .catch(error => {
                     console.log(error)
                 })
+            this.$store.commit('setIsLoading', false)
         },
         addToCart() {
             console.log('addToCart')
